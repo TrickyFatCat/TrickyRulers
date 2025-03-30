e64769218d997c000709956e163549a709d99a71
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-30 19:33:18 +0300

    Create TrickyRuler class

---
+++ Source/TrickyRulers/Private/TrickyRuler.cpp
@@ -0,0 +1,54 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "TrickyRuler.h"
+
+
+ATrickyRuler::ATrickyRuler()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bIsEditorOnlyActor = true;
+
+	RootComponent = CreateEditorOnlyDefaultSubobject<USceneComponent>(TEXT("Root"));
+}
+
+bool ATrickyRuler::ShouldTickIfViewportsOnly() const
+{
+	return true;
+}
+
+void ATrickyRuler::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	LineRuler.UpdateLength();
+}
+
+void ATrickyRuler::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ATrickyRuler::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	switch (RulerType)
+	{
+	case ERulerType::Line:
+		DrawLineRuler(GetActorForwardVector(), LineRuler.Length, LineRuler.Color, LineRuler.Thickness);
+		break;
+	default:
+		break;
+	}
+}
+
+void ATrickyRuler::DrawLineRuler(const FVector& Direction,
+                                 const float Length,
+                                 const FColor& Color,
+                                 const float Thickness) const
+{
+	const FVector LineStart = GetActorLocation();
+	const FVector LineEnd = LineStart + Direction * Length;
+	DrawDebugLine(GetWorld(), LineStart, LineEnd, Color, false, 0.f, 0, Thickness);
+}
---
+++ Source/TrickyRulers/Public/TrickyRuler.h
@@ -0,0 +1,78 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TrickyRuler.generated.h"
+
+UENUM()
+enum class ERulerType : uint8
+{
+	Line,
+	Circle,
+	Sphere,
+	Cylinder,
+	Capsule,
+	Box,
+};
+
+USTRUCT(BlueprintType)
+struct FLineRulerProperties
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere,
+		Category="Ruler",
+		meta=(Units="cm", ClampMin=1, UIMin=1))
+	int32 Length = 100;
+
+	UPROPERTY(VisibleAnywhere, Category="Ruler")
+	FString Meters = TEXT("1 m");
+
+	UPROPERTY(EditAnywhere,
+		Category="Ruler",
+		meta=(ClampMin=1.0f, ClampMax=10.0f, UIMin=1.0f, UIMax=10.0f, Delta=1.0f))
+	float Thickness = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Ruler", meta=(HideAlphaChannel))
+	FColor Color = FColor::Red;
+
+	void UpdateLength()
+	{
+		const float LengthMeters = static_cast<float>(Length) / 100.f;
+		Meters = FString::Printf(TEXT("%.2f m"), LengthMeters);
+	}
+};
+
+UCLASS(HideCategories=(Collision, Actor, Input, Rendering, Replication, Cooking, HLOD, LevelInstance, DataLayers, Networking, WorldPartition, Physics))
+class TRICKYRULERS_API ATrickyRuler : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ATrickyRuler();
+
+protected:
+	virtual bool ShouldTickIfViewportsOnly() const override;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category="Ruler")
+	ERulerType RulerType = ERulerType::Line;
+
+	// Line ruler properties
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category="Ruler",
+		meta=(EditCondition="RulerType==ERulerType::Line", EditConditionHides))
+	FLineRulerProperties LineRuler;
+
+private:
+	UFUNCTION()
+	void DrawLineRuler(const FVector& Direction, const float Length, const FColor& Color, const float Thickness) const;
+};
