08aef483d13037e76403107fdc3b2642fb943846
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-30 19:48:39 +0300

    Update TrickyRuler

--- Source/TrickyRulers/Public/TrickyRuler.h
+++ Source/TrickyRulers/Public/TrickyRuler.h
@@ -4,46 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "TrickyRulerProperties.h"
 #include "TrickyRuler.generated.h"
-
-UENUM()
-enum class ERulerType : uint8
-{
-	Line,
-	Circle,
-	Sphere,
-	Cylinder,
-	Capsule,
-	Box,
-};
-
-USTRUCT(BlueprintType)
-struct FLineRulerProperties
-{
-	GENERATED_BODY()
-
-	UPROPERTY(EditAnywhere,
-		Category="Ruler",
-		meta=(Units="cm", ClampMin=1, UIMin=1))
-	int32 Length = 100;
-
-	UPROPERTY(VisibleAnywhere, Category="Ruler")
-	FString Meters = TEXT("1 m");
-
-	UPROPERTY(EditAnywhere,
-		Category="Ruler",
-		meta=(ClampMin=1.0f, ClampMax=10.0f, UIMin=1.0f, UIMax=10.0f, Delta=1.0f))
-	float Thickness = 1.0f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Ruler", meta=(HideAlphaChannel))
-	FColor Color = FColor::Red;
-
-	void UpdateLength()
-	{
-		const float LengthMeters = static_cast<float>(Length) / 100.f;
-		Meters = FString::Printf(TEXT("%.2f m"), LengthMeters);
-	}
-};
 
 UCLASS(HideCategories=(Collision, Actor, Input, Rendering, Replication, Cooking, HLOD, LevelInstance, DataLayers, Networking, WorldPartition, Physics))
 class TRICKYRULERS_API ATrickyRuler : public AActor
