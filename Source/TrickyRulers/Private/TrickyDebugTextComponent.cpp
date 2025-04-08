9071719813f8109f1a026f206bcbb648d3022149
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-04-08 22:01:36 +0300

    Create debug text component

---
+++ Source/TrickyRulers/Private/TrickyDebugTextComponent.cpp
@@ -0,0 +1,141 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "TrickyDebugTextComponent.h"
+
+#include "Engine/Canvas.h"
+
+FDebugSceneProxy::FDebugSceneProxy(const UPrimitiveComponent* InComponent,
+                                   FDebugSceneProxyData* ProxyData) : FDebugRenderSceneProxy(InComponent)
+{
+	this->ProxyData = *ProxyData;
+
+	DrawType = EDrawType::SolidAndWireMeshes;
+	ViewFlagName = "Editor";
+
+	if (ProxyData->bDrawInGame && (InComponent->GetWorld() && InComponent->GetWorld()->IsGameWorld()))
+	{
+		ViewFlagName = "Game";
+	}
+
+
+	for (const auto& Text : ProxyData->DebugLabels)
+	{
+		this->Texts.Add({
+			Text.Text,
+			Text.Location,
+			Text.Color,
+		});
+	}
+}
+
+void FDebugTextDelegateHelper::DrawDebugLabels(UCanvas* Canvas, APlayerController* PlayerController)
+{
+	if (!Canvas || !bDrawDebug)
+	{
+		return;
+	}
+
+	const FDebugSceneProxyData::FDebugText* DebugText = DebugLabels.GetData();
+	const FColor OldDrawColor = Canvas->DrawColor;
+	Canvas->SetDrawColor(DebugText->Color);
+	const FSceneView* View = Canvas->SceneView;
+	const UFont* Font = GEngine->GetSmallFont();
+
+	FFontRenderInfo FontRenderInfo;
+	FontRenderInfo.bEnableShadow = true;
+
+	for (int32 i = 0; i < DebugLabels.Num(); ++i, ++DebugText)
+	{
+		if (View->ViewFrustum.IntersectSphere(DebugText->Location, 1.0f))
+		{
+			const FVector ScreenLoc = Canvas->Project(DebugText->Location);
+			Canvas->DrawText(Font,
+			                 DebugText->Text,
+			                 ScreenLoc.X,
+			                 ScreenLoc.Y,
+			                 DebugText->Scale,
+			                 DebugText->Scale,
+			                 FontRenderInfo);
+		}
+	}
+
+	Canvas->SetDrawColor(OldDrawColor);
+}
+
+void FDebugTextDelegateHelper::SetupFromProxy(const FDebugSceneProxy* InSceneProxy)
+{
+	DebugLabels.Reset();
+	DebugLabels.Append(InSceneProxy->ProxyData.DebugLabels);
+}
+
+UTrickyDebugTextComponent::UTrickyDebugTextComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	bIsEditorOnly = true;
+}
+
+FDebugRenderSceneProxy* UTrickyDebugTextComponent::CreateDebugSceneProxy()
+{
+	FDebugSceneProxyData ProxyData;
+	ProxyData.bDrawInGame = bDrawInGame;
+
+	auto AddLabelData = [&](const FTrickyDebugTextData& Label)-> void
+	{
+		FVector Location = Label.bUseCustomLocation ? Label.Location : GetComponentLocation();
+		ProxyData.DebugLabels.Add({Label, Location});
+	};
+
+	if (DebugLabels.Num() == 0)
+	{
+		return nullptr;
+	}
+
+	for (const FTrickyDebugTextData& Label : DebugLabels)
+	{
+		AddLabelData(Label);
+	}
+
+	FDebugSceneProxy* DebugSceneProxy = new FDebugSceneProxy(this, &ProxyData);
+
+	DebugDrawDelegateManager.bDrawDebug = bDrawDebug;
+
+	if (DebugSceneProxy)
+	{
+		DebugDrawDelegateManager.SetupFromProxy(DebugSceneProxy);
+	}
+
+	return DebugSceneProxy;
+}
+
+FBoxSphereBounds UTrickyDebugTextComponent::CalcBounds(const FTransform& LocalToWorld) const
+{
+	return FBoxSphereBounds(FBox(FVector(-1000, -1000, -1000), FVector(1000, 1000, 1000)));
+}
+
+void UTrickyDebugTextComponent::SetDebugLabel(const FTrickyDebugTextData& LabelData)
+{
+	DebugLabels.Empty();
+	DebugLabels.Add(LabelData);
+	MarkRenderStateDirty();
+}
+
+void UTrickyDebugTextComponent::SetDebugLabels(const TArray<FTrickyDebugTextData>& LabelsData)
+{
+	DebugLabels.Empty();
+	DebugLabels = LabelsData;
+	MarkRenderStateDirty();
+}
+
+void UTrickyDebugTextComponent::SetDrawDebug(const bool Value)
+{
+	bDrawDebug = Value;
+	MarkRenderStateDirty();
+}
+
+void UTrickyDebugTextComponent::SetDrawInGame(const bool Value)
+{
+	bDrawInGame = Value;
+	bIsEditorOnly = !bDrawInGame;
+	MarkRenderStateDirty();
+}
---
+++ Source/TrickyRulers/Public/TrickyDebugTextComponent.h
@@ -0,0 +1,116 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Debug/DebugDrawComponent.h"
+#include "TrickyDebugTextComponent.generated.h"
+
+USTRUCT(BlueprintType)
+struct FTrickyDebugTextData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="DebugText")
+	FString Text = "DebugText";
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="DebugText")
+	bool bUseCustomLocation = false;
+
+	UPROPERTY(EditAnywhere,
+		BlueprintReadWrite,
+		Category="DebugText",
+		meta=(EditCondition="bUseCustomLocation", EditConditionHides))
+	FVector Location{FVector::ZeroVector};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="DebugText")
+	FLinearColor Color{FColor::Magenta};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="DebugText")
+	float TextScale = 1.f;
+};
+
+struct FDebugSceneProxyData
+{
+	struct FDebugText
+	{
+		FString Text;
+		FVector Location;
+		FColor Color;
+		float Scale;
+
+		FDebugText(): Location(FVector::ZeroVector), Color(FColor::Magenta), Scale(1)
+		{
+		}
+
+		FDebugText(const FTrickyDebugTextData& Label, const FVector& InLocation)
+			: Text(Label.Text), Location(InLocation), Color(Label.Color.ToFColor(false)), Scale(Label.TextScale)
+		{
+		}
+	};
+
+	bool bDrawInGame = false;
+	TArray<FDebugText> DebugLabels;
+};
+
+class FDebugSceneProxy : public FDebugRenderSceneProxy
+{
+public:
+	FDebugSceneProxy(const UPrimitiveComponent* InComponent, FDebugSceneProxyData* ProxyData);
+
+	FDebugSceneProxyData ProxyData;
+};
+
+class FDebugTextDelegateHelper : public FDebugDrawDelegateHelper
+{
+public:
+	virtual void DrawDebugLabels(UCanvas* Canvas, APlayerController*) override;
+
+	void SetupFromProxy(const FDebugSceneProxy* InSceneProxy);
+
+	bool bDrawDebug = true;
+
+	TArray<FDebugSceneProxyData::FDebugText> DebugLabels;
+};
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TRICKYRULERS_API UTrickyDebugTextComponent : public UDebugDrawComponent
+{
+	GENERATED_BODY()
+
+public:
+	UTrickyDebugTextComponent();
+
+protected:
+	FDebugTextDelegateHelper DebugDrawDelegateManager;
+
+	UPROPERTY(EditAnywhere, Category="DebugText")
+	bool bDrawDebug = true;
+
+	UPROPERTY(EditAnywhere, Category="DebugText",
+		meta=(EditCondition = "!bDrawOneLabel && bDrawDebug", EditConditionHides))
+	TArray<FTrickyDebugTextData> DebugLabels;
+
+	UPROPERTY(EditAnywhere, Category="DebugText", meta=(EditCondition="bDrawDebug"))
+	bool bDrawInGame = false;
+
+	virtual FDebugRenderSceneProxy* CreateDebugSceneProxy() override;
+
+	virtual FDebugDrawDelegateHelper& GetDebugDrawDelegateHelper() override { return DebugDrawDelegateManager; }
+
+	virtual FBoxSphereBounds CalcBounds(const FTransform& LocalToWorld) const override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category="DebugText")
+	void SetDebugLabel(const FTrickyDebugTextData& LabelData);
+
+	UFUNCTION(BlueprintCallable, Category="DebugText")
+	void SetDebugLabels(const TArray<FTrickyDebugTextData>& LabelsData);
+
+	UFUNCTION(BlueprintCallable, Category="DebugText")
+	void SetDrawDebug(const bool Value);
+
+	UFUNCTION(BlueprintCallable, Category="DebugText")
+	void SetDrawInGame(const bool Value);
+};
