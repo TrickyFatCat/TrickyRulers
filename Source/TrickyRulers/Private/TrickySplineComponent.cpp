bb0b35a040c2578a822b310a10f5f37471796100
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-04-12 15:06:29 +0300

    Create TrickySplineComponent to hide some categories

---
+++ Source/TrickyRulers/Private/TrickySplineComponent.cpp
@@ -0,0 +1,12 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "TrickySplineComponent.h"
+
+
+UTrickySplineComponent::UTrickySplineComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bIsEditorOnly = true;
+	Mobility = EComponentMobility::Static;
+}
---
+++ Source/TrickyRulers/Private/TrickySplineComponent.h
@@ -0,0 +1,18 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SplineComponent.h"
+#include "TrickySplineComponent.generated.h"
+
+
+UCLASS(HideCategories = (Collision, Activation, Navigation, Cooking, LOD, TextureStreaming, HLOD, Tags, RayTracing,
+	AssetUserData, ComponentTick, ComponentReplication, Events, Replication, Sockets, Variable, Transform))
+class TRICKYRULERS_API UTrickySplineComponent : public USplineComponent
+{
+	GENERATED_BODY()
+
+public:
+	UTrickySplineComponent();
+};
